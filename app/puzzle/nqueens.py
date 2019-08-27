1544e283bbf00115aae7a3d1c9268165ed6b5733
Jyr Gaxiola <jyr.gaxiola@gmail.com>
2019-08-27 12:26:37 -0500

    feat(solutions): determine all possible solutions (within 10 mins on a laptop)

--- app/puzzle/nqueens.py
+++ app/puzzle/nqueens.py
@@ -5,6 +5,8 @@ try:
 except ModuleNotFoundError:
     from puzzle.db.models import SolutionModel
 
+import timeout_decorator
+
 class Play:
 
     def __init__(self, N = 8, verbose = False, record = False):
@@ -36,15 +38,19 @@ class Play:
                     self.diagonal_asc[ diagonal_up_index]):
                     self.queens_positions[column] = row
 
-                    self.row_is_free[row] = False
-                    self.diagonal_desc[ diagonal_down_index ] = False
-                    self.diagonal_asc[ diagonal_up_index ] = False
-
+                    self.flag_toggle(
+                        row=row,
+                        ddi=diagonal_down_index,
+                        dup=diagonal_up_index,
+                        bool=False
+                    )
                     self.solve(column+1)
-
-                    self.row_is_free[row] = True
-                    self.diagonal_desc[ diagonal_down_index ] = True
-                    self.diagonal_asc[ diagonal_up_index ] = True
+                    self.flag_toggle(
+                        row=row,
+                        ddi=diagonal_down_index,
+                        dup=diagonal_up_index,
+                        bool=True
+                    )
 
     def threatened_cells(self):
         return [True for i in range((self.N * 2) - 1)]
@@ -54,6 +60,16 @@ class Play:
         for row in range(self.N):
             position = self.queens_positions[row] + 1
             self.positions = "{} {}".format(self.positions, position)
+
+    def flag_toggle(self, **kwargs):
+        row = kwargs['row']
+        diagonal_down_index = kwargs['ddi']
+        diagonal_up_index = kwargs['dup']
+        bool = kwargs['bool']
+
+        self.row_is_free[row] = bool
+        self.diagonal_desc[ diagonal_down_index ] = bool
+        self.diagonal_asc[ diagonal_up_index ] = bool
 
     def record(self):
         if self.activate_record == True:
@@ -71,10 +87,15 @@ class Play:
                     end = " " if row<self.N-1 else "\n"
                 )
 
+@timeout_decorator.timeout(600)
 def main():
+    print("Start")
     puzzle = Play(N = 8, verbose = True, record=True)
     puzzle.solve()
     print("{} solutions".format(puzzle.solution_number))
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except timeout_decorator.timeout_decorator.TimeoutError:
+       print("Timed Out - 10 MINUTES TO EXECUTE IT")
